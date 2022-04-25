f94e32e15c906a7a8283161e5c6e958fad788e09
devvv <dsevrens@gmail.com>
2022-04-24 21:24:22 -0700

    actually fixed versioning and bstats errors

---
+++ src/main/java/ca/rpgcraft/damageindicatorsplus/ProtocolLibHandler.java
@@ -0,0 +1,45 @@
+package ca.rpgcraft.damageindicatorsplus;
+
+import ca.rpgcraft.damageindicatorsplus.tasks.GenerateVectorTask;
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+
+public class ProtocolLibHandler {
+
+    private final DamageIndicatorsPlus plugin;
+    private final GenerateVectorTask generateVectorTask;
+
+    public ProtocolLibHandler(DamageIndicatorsPlus plugin, GenerateVectorTask generateVectorTask){
+        this.plugin = plugin;
+        this.generateVectorTask = generateVectorTask;
+        initProtocolLib();
+    }
+
+    public void initProtocolLib(){
+        plugin.getLogger().info("ProtocolLib found.");
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(plugin, PacketType.Play.Server.SPAWN_ENTITY_LIVING) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                PacketContainer packet = event.getPacket();
+                Player p = event.getPlayer();
+
+                int entityID = packet.getIntegers().read(0);
+                Entity entity = ProtocolLibrary.getProtocolManager().getEntityFromID(p.getWorld(), entityID);
+
+                if(!(entity instanceof ArmorStand)) return;
+                ArmorStand hologram = (ArmorStand) entity;
+                if(!(hologram.getEntitySpawnReason().equals(CreatureSpawnEvent.SpawnReason.BEEHIVE))) return;
+
+                hologram.setVisible(false);
+                hologram.setVelocity(generateVectorTask.generateVector());
+            }
+        });
+    }
+}
